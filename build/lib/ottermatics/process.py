4653269bb3bc0d71b1eff5c7261a69b8e31d81f5
SoundsSerious <keavin7@yahoo.com>
2020-09-12 12:59:11 -0400

    remove file changes

--- build/lib/ottermatics/process.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import *
-from patterns import *
-
-from deco import *
-
-class AutoProcess(object):
-    '''Uses Deco Library To Parallalize Functions using @concurrent decorator'''
-        __metaclass__ = Singleton
