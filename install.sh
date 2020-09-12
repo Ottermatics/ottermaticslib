8840622039925c7777cbe1334137065c4f098326
SoundsSerious <keavin7@yahoo.com>
2020-09-12 14:00:08 -0400

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,39 @@
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+
 sudo apt-get install git
+sudo apt install gcc
+sudo apt install g++
+sudo apt-get install build-essential
+
+
+sudo apt-get install ffmpeg
+
+# Install necessary system packages
+sudo apt-get install -y \
+    ffmpeg \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-ttf-dev \
+    libportmidi-dev \
+    libswscale-dev \
+    libavformat-dev \
+    libavcodec-dev \
+    zlib1g-dev
+
+# Install gstreamer for audio, video (optional)
+sudo apt-get install -y \
+    libgstreamer1.0 \
+    gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-good
 
 echo 'Installing Python3'
-sudo apt-get install python3 python3-dev python3-setuptools
+sudo apt-get install python3 python3-pip python3-dev python3-setuptools
 python3 -m pip install --upgrade pip
 pip3 install virtualenv
+
+
 
 echo 'Installing Anaconda Python (follow instructions, agree & yes)'
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
@@ -25,6 +55,7 @@ cat < ./.ssh/id_rsa.pub
 echo 'Installing Ottermatics Lib'
 git clone git@github.com:SoundsSerious/ottermaticslib.git
 cd ottermaticslib
+python -m pip install -r requirements.txt
 python setup.py install
 
  
--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ Created on Sat May 11 22:38:11 2019
 
 from setuptools import setup
 
-def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
+# def parse_requirements(filename):
+#     """ load requirements from a pip requirements file """
+#     lineiter = (line.strip() for line in open(filename))
+#     return [line for line in lineiter if line and not line.startswith("#")]
 
-install_reqs = parse_requirements('requirements.txt')
+# install_reqs = parse_requirements('requirements.txt')
 
 setup(name='Ottermatics Lib',
       version='0.1',
@@ -22,5 +22,4 @@ setup(name='Ottermatics Lib',
       author='Olly',
       author_email='olly@ottermatics.com',
       license='MIT',
-      install_requires = install_reqs,
       zip_safe=False)
