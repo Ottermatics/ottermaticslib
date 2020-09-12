ade018683cf09142d8f5adce2861aa9fa83e76f4
SoundsSerious <keavin7@yahoo.com>
2020-09-12 14:04:46 -0400

    dont install kivy

--- install.sh
+++ install.sh
@@ -7,26 +7,27 @@ sudo apt install g++
 sudo apt-get install build-essential
 
 
-sudo apt-get install ffmpeg
+# sudo apt-get install ffmpeg
 
-# Install necessary system packages
-sudo apt-get install -y \
-    ffmpeg \
-    libsdl2-dev \
-    libsdl2-image-dev \
-    libsdl2-mixer-dev \
-    libsdl2-ttf-dev \
-    libportmidi-dev \
-    libswscale-dev \
-    libavformat-dev \
-    libavcodec-dev \
-    zlib1g-dev
+# # Install necessary system packages
+# sudo apt-get install -y gir1.2-pangoft2-1.0
+# sudo apt-get install -y \
+#     ffmpeg \
+#     libsdl2-dev \
+#     libsdl2-image-dev \
+#     libsdl2-mixer-dev \
+#     libsdl2-ttf-dev \
+#     libportmidi-dev \
+#     libswscale-dev \
+#     libavformat-dev \
+#     libavcodec-dev \
+#     zlib1g-dev
 
-# Install gstreamer for audio, video (optional)
-sudo apt-get install -y \
-    libgstreamer1.0 \
-    gstreamer1.0-plugins-base \
-    gstreamer1.0-plugins-good
+# # Install gstreamer for audio, video (optional)
+# sudo apt-get install -y \
+#     libgstreamer1.0 \
+#     gstreamer1.0-plugins-base \
+#     gstreamer1.0-plugins-good
 
 echo 'Installing Python3'
 sudo apt-get install python3 python3-pip python3-dev python3-setuptools
