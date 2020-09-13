e05980ece138f6f2b5625e06428fb38f38656455
SoundsSerious <keavin7@yahoo.com>
2020-09-13 13:22:57 -0400

    remove ffmpeg stuff

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ mkdir sw
 cd sw
 if grep -q Microsoft /proc/version; then
   #Install SYSTEMCTL
+  echo "Install WSL Stuff... systemctl"
   git clone https://github.com/DamionGans/ubuntu-wsl2-systemd-script.git
   bash ubuntu-wsl2-systemd-script/ubuntu-wsl2-systemd-script.sh
 else
@@ -20,28 +21,6 @@ else
 fi
 
 cd ~/
-
-# sudo apt-get install ffmpeg
-
-# # Install necessary system packages
-# sudo apt-get install -y gir1.2-pangoft2-1.0
-# sudo apt-get install -y \
-#     ffmpeg \
-#     libsdl2-dev \
-#     libsdl2-image-dev \
-#     libsdl2-mixer-dev \
-#     libsdl2-ttf-dev \
-#     libportmidi-dev \
-#     libswscale-dev \
-#     libavformat-dev \
-#     libavcodec-dev \
-#     zlib1g-dev
-
-# # Install gstreamer for audio, video (optional)
-# sudo apt-get install -y \
-#     libgstreamer1.0 \
-#     gstreamer1.0-plugins-base \
-#     gstreamer1.0-plugins-good
 
 echo 'Installing Python3'
 sudo apt-get install python3 python3-pip python3-dev python3-setuptools
