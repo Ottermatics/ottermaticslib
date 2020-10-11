f58f65ef212f02ad73f5a9b6e2acea930b8090ed
SoundsSerious <keavin7@yahoo.com>
2020-10-11 17:26:52 -0400

    small changes

--- ottermatics/data.py
+++ ottermatics/data.py
@@ -83,14 +83,14 @@ def is_ec2_instance():
     return False   
 
 #connection_string = "host='{host}' user='{user}'  password='{passd}'".format(host=HOST,user=USER,passd=PASS)
-def configure_db_connection(database_name,host=HOST,user=USER,passd=PASS,port=PORT):
+def configure_db_connection(database_name,host=HOST,user=USER,passd=PASS,port=PORT,pool_size=20, max_overflow=0):
     '''Returns Scoped Session, engine, and a connection_string'''
     connection_string = "postgresql://{user}:{passd}@{host}:{port}/{database}"\
                         .format(host=host,user=user,passd=passd,port=port,database=database_name)
 
     print('Connecting with {}'.format(connection_string))
 
-    engine = create_engine(connection_string)
+    engine = create_engine(connection_string,pool_size=pool_size, max_overflow=max_overflow)
     engine.echo = False
 
     scopefunc = functools.partial(context.get, "uuid")
