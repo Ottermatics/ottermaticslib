7e58e2a3dc3a247508de4470a6543c4bd33a5de7
SoundsSerious <keavin7@ottermatics.com>
2021-05-03 20:38:54 -0400

    Ok we have a working install requirements

--- setup.py
+++ setup.py
@@ -8,15 +8,16 @@ Created on Sat May 11 22:38:11 2019
 
 from setuptools import setup
 
-# def parse_requirements(filename):
-#     """ load requirements from a pip requirements file """
-#     lineiter = (line.strip() for line in open(filename))
-#     return [line for line in lineiter if line and not line.startswith("#")]
 
-# install_reqs = parse_requirements('requirements.txt')
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+install_reqs = parse_requirements('requirements.txt')
 
 setup(name='ottermatics',
-      version='0.1',
+      version='0.2',
       description='The Ottermatic\'s Python Lib!',
       url='',
       author='Olly',
@@ -31,4 +32,5 @@ setup(name='ottermatics',
                 'otterdrive=ottermatics.gdocs:main_cli'
             ]
       },
+      install_requires = install_reqs,
       zip_safe=False)
